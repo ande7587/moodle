f071d52bfbe8df4e61d9f15eb42f6074abafd2a2
Clement Smith <cdsmith@oit-moodle-dev-28.oit.umn.edu>
2015-01-29 14:49:44 -0600

    Add blocks/massaction. Copied from 2.6. This is our custom block.

---
+++ blocks/massaction/version.php
@@ -0,0 +1,5 @@
+<?php
+$plugin->version = 2014081900;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2012110900; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'Mass Actions Block';
