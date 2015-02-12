1558f8babf01974aa2456a5789fb5e0d5f13ecca
jinhofer <jinhofer@umn.edu>
2015-02-12 16:52:26 -0600

    theme/umn_clean banner update
    theme/umn_clean: MTE-235: Made our banner play nicely with Moodle code

--- theme/umn_clean/layout/banner.php
+++ theme/umn_clean/layout/banner.php
@@ -69,7 +69,7 @@ if (!empty($PAGE->theme->settings->custom_values['logotitle'])) {
 
 <header role="banner" class="moodle-has-zindex <?php if (isset($CFG->environment)) { echo $CFG->environment; } ?>">
         <div id="header-banner">
-            <div id="header-img-wrap">
+            <div id="header-img-wrap" class="navbar">
             <div id="header-img">
                 <div id="my-courses" tabindex="0" title="<?php echo get_string('my-courses-button','theme_umn_clean'); ?>">
                 </div>
