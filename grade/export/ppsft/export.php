ande7587/moodle

commit c0bd9b29ef44151d7242f83c96efebeeb9023b9a

PeopleSoft grade export custom plugin.
Copied from 2.6.
Updated deprecated constructors and methods.
Removed preview page to comply with other 'export to' plugins.

grade/export/ppsft/export.php
@@ -1,0 +1,26 @@
+<?php
+
+require_once '../../../config.php';
+require_once $CFG->dirroot.'/grade/export/lib.php';
+require_once 'grade_export_ppsft.php';
+require_once 'ppsft_grade_export_form.php';
+$id = required_param('id', PARAM_INT); // course id
+
+if (!$course = $DB->get_record('course', array('id'=>$id))) {
+    print_error('nocourseid');
+}
+
+require_login($course);
+$context = context_course::instance($id);
+
+require_capability('moodle/grade:export', $context);
+require_capability('gradeexport/ppsft:view', $context);
+
+// print all the exported data here
+$classes = grade_export_ppsft::get_graded_ppsft_classes_for_course($COURSE->id);
+
+//pass list of peoplesoft classes to be selected  by user
+$mform = new ppsft_grade_export_form(null, array( 'classes'=>$classes ));
+$export = new grade_export_ppsft($course, $mform->get_data());
+$export->print_grades();
+

grade/export/ppsft/db/access.php
@@ -1,0 +1,27 @@
+<?php
+
+/**
+ * Capabilities gradeexport plugin.
+ *
+ * @package    gradeexport_ppsft
+ * @author     Colin Campbell (University of Minnesota) 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+
+    'gradeexport/ppsft:view' => array(
+        'riskbitmask' => RISK_PERSONAL,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    )
+);
+
+

grade/export/ppsft/grade_export_ppsft.php
@@ -1,0 +1,226 @@
+<?php
+
+require_once($CFG->dirroot.'/grade/export/lib.php');
+require_once($CFG->libdir . '/csvlib.class.php');
+require_once('ppsft_grade_export_iterator.php');
+
+/**
+ * Overriding csv_export_writer so that we can adjust file naming.
+ */
+class ppsft_csv_export_writer extends csv_export_writer {
+
+    /**
+     * Set the filename for the uploaded csv file.  Overridden from base class.
+     *
+     * @param string $dataname    The name of the module.
+     * @param string $extension  File extension for the file.
+     */
+    public function set_filename($dataname, $extension = '.csv') {
+        $filename = clean_filename($dataname);
+        $filename .= clean_filename('-' . gmdate("Ymd_Hi"));
+        $filename .= $extension;
+        $this->filename = $filename;
+    }
+
+}
+
+class grade_export_ppsft extends grade_export {
+
+    public $plugin = 'ppsft';
+
+    public $separator = 'comma';
+
+    public $includeheaders;
+    public $includename;
+    public $includegradingbasis;
+    public $ppsftclassid;
+
+    /**
+     * Constructor should set up all the private variables ready to be pulled
+     * @param object $course
+     * @param int $ppsftclassid of selected ppsft section, 0 means all
+     * @param string $itemlist comma separated list of item ids, empty means all
+     */
+    public function __construct($course, $formdata){
+        parent::__construct($course, 0, $formdata);
+        $this->includeheaders = $formdata->includeheaders;
+        $this->includename = $formdata->includename;
+        $this->includegradingbasis = $formdata->includegradingbasis;
+        $this->ppsftclassid = $formdata->ppsftclassid;
+    }
+
+    /**
+     * Init object based using data from form
+     * @param object $formdata
+     */
+    function process_form($formdata) {
+        parent::process_form($formdata);
+
+        // The following are assumed to be in the form, so not testing with isset.
+        $this->includeheaders = $formdata->includeheaders;
+        $this->includename = $formdata->includename;
+        $this->includegradingbasis = $formdata->includegradingbasis;
+    }
+
+    /**
+     * If the grade is null, we want an empty string rather than the hyphen
+     * that the parent implementation provides.
+     */
+    public function format_grade($grade, $gradedisplayconst = null) {
+        if (is_null($grade->finalgrade)) {
+            return '';
+        }
+        return parent::format_grade($grade, $gradedisplayconst);
+    }
+
+    /**
+     * Returns array of parameters used by dump.php and export.php.
+     * Overridden from base.
+     * @return array
+     */
+    public function get_export_params() {
+        $itemids = array_keys($this->columns);
+        $itemidsparam = implode(',', $itemids);
+        if (empty($itemidsparam)) {
+            $itemidsparam = '-1';
+        }
+
+        # TODO: Which do we no longer need?
+        $params = array('id'                =>$this->course->id,
+                        'ppsftclassid'      =>$this->ppsftclassid,
+                        'itemids'           =>$itemidsparam,
+                        'export_letters'    =>$this->export_letters,
+                        'includeheaders'    =>$this->includeheaders,
+                        'includename'       =>$this->includename,
+                        'includegradingbasis' =>$this->includegradingbasis
+                        );
+
+        return $params;
+    }
+
+    private function get_date_string($time) {
+        $datestr = $time ? strftime('%m/%d/%Y', $time) : '';
+        return $datestr;
+    }
+    public function print_grades() {
+        global $CFG;
+
+        $downloadfilename = $this->get_download_filename();
+        $csvexport = new ppsft_csv_export_writer($this->separator);
+        $csvexport->set_filename($downloadfilename);
+
+        if ($this->includeheaders) {
+
+            // Print names of all the fields
+            $exporttitle = array();
+
+            $exporttitle[] = get_string('columnidnumber', 'gradeexport_ppsft');
+
+            // Should be just the course total for this report
+            // but keeping the loop for now.
+            foreach ($this->columns as $grade_item) {
+                // Looping over displaytype as other export (e.g., txt) do, but in
+                // our case it will likely always be letter displaytype.
+                foreach ($this->displaytype as $gradedisplayname => $gradedisplayconst) {
+                    $exporttitle[] = $this->format_column_name($grade_item, false, $gradedisplayname);
+                }
+            }
+
+            $exporttitle[] = get_string('lastaccess', 'gradeexport_ppsft');
+
+            if ($this->includename) {
+                $exporttitle[] = get_string('columnname', 'gradeexport_ppsft');
+            }
+
+            if ($this->includegradingbasis) {
+                $exporttitle[] = get_string('columngradingbasis', 'gradeexport_ppsft');
+            }
+
+            $csvexport->add_data($exporttitle);
+        }
+
+        // Print all the lines of data.
+        $gui = new ppsft_grade_export_iterator($this->course, $this->columns, $this->ppsftclassid);
+        $gui->init();
+        while ($userdata = $gui->next_user()) {
+
+            $exportdata = array();
+            $user = $userdata->user;
+
+            $exportdata[] = $user->idnumber;
+
+            foreach ($userdata->grades as $itemid => $grade) {
+                // Looping over displaytype as other export (e.g., txt) do, but in
+                // our case it will likely always be letter displaytype.
+                foreach ($this->displaytype as $gradedisplayconst) {
+                    $exportdata[] = $this->format_grade($grade, $gradedisplayconst);
+                }
+            }
+            $exportdata[] = $this->get_date_string($user->last_access);
+
+            if ($this->includename) {
+                $exportdata[] = "{$user->lastname}, {$user->firstname}";
+            }
+
+            if ($this->includegradingbasis) {
+                $exportdata[] = $user->ppsft_grading_basis;
+            }
+
+            $csvexport->add_data($exportdata);
+        }
+        $gui->close();
+        $csvexport->download_file();
+        exit;
+    }
+    /**
+     *
+     */
+    private function get_download_filename() {
+        global $DB;
+
+        $sql =<<<SQL
+select
+    concat(pc.term, pc.institution, pc.class_nbr, '_', pc.subject, '_',
+              pc.catalog_nbr, '_', pc.section) as basename
+from {ppsft_classes} pc
+where pc.id = :ppsftclassid
+SQL;
+
+        $basename = $DB->get_field_sql($sql, array('ppsftclassid' => $this->ppsftclassid));
+
+        // The csv_export_writer will add the suffix to the filename.
+        return $basename;
+    }
+
+    /**
+     *
+     */
+    private function get_graded_ppsft_classes() {
+        return static::get_graded_ppsft_classes_for_course($this->course->id);
+    }
+
+    public static function get_graded_ppsft_classes_for_course($courseid) {
+        global $DB;
+
+        $sql =<<<SQL
+select
+    pc.id,
+    pc.subject,
+    pc.catalog_nbr,
+    pc.section
+from {enrol} e
+  join {enrol_umnauto_classes} uc on e.id=uc.enrolid
+  join {ppsft_classes} pc on pc.id=uc.ppsftclassid
+where e.courseid = :courseid and
+      exists (select id from {ppsft_class_enrol} pce
+              where pce.ppsftclassid=pc.id and
+                    pce.status='E' and
+                    pce.grading_basis not in ('NON', 'NGA'))
+order by pc.term, pc.institution, pc.subject, pc.catalog_nbr, pc.section
+SQL;
+
+        return $DB->get_records_sql($sql, array('courseid' => $courseid));
+    }
+}
+
+

grade/export/ppsft/index.php
@@ -1,0 +1,44 @@
+<?php
+
+require_once '../../../config.php';
+require_once $CFG->dirroot.'/grade/export/lib.php';
+require_once 'grade_export_ppsft.php';
+require_once 'ppsft_grade_export_form.php';
+
+$id = required_param('id', PARAM_INT); // course id
+
+$PAGE->set_url('/grade/export/ppsft/index.php', array('id'=>$id));
+
+if (!$course = $DB->get_record('course', array('id'=>$id))) {
+    print_error('nocourseid');
+}
+
+require_login($course);
+$context = context_course::instance($id);
+
+require_capability('moodle/grade:export', $context);
+require_capability('gradeexport/ppsft:view', $context);
+
+print_grade_page_head($COURSE->id, 'export', 'ppsft', get_string('exportto', 'grades') . ' ' . get_string('pluginname', 'gradeexport_ppsft'));
+
+$classes = grade_export_ppsft::get_graded_ppsft_classes_for_course($COURSE->id);
+if (empty($classes)) {
+    echo $OUTPUT->box(get_string('nogradableclasses', 'gradeexport_ppsft'));
+    echo $OUTPUT->footer();
+    exit;
+}
+
+export_verify_grades($COURSE->id);
+$actionurl = new moodle_url('/grade/export/ppsft/export.php');
+
+$mform = new ppsft_grade_export_form($actionurl, array( 'classes'=>$classes ));
+echo '<div class="clearer"></div>';
+
+echo $OUTPUT->container_start('ppsftgradeexportinstructions');
+echo get_string('firstpageexportinstructions', 'gradeexport_ppsft');
+echo $OUTPUT->container_end();
+
+$mform->display();
+
+echo $OUTPUT->footer();
+

grade/export/ppsft/lang/en/gradeexport_ppsft.php
@@ -1,0 +1,34 @@
+<?php
+
+/**
+ * Strings for component 'gradeexport_ppsft', language 'en'
+ *
+ * @package   gradeexport_ppsft
+ * @author    Colin Campbell (University of Minnesota)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'PeopleSoft upload file';
+$string['ppsft:publish'] = 'Publish grade export for PeopleSoft upload';
+$string['ppsft:view'] = 'Use grade export for PeopleSoft upload';
+
+$string['ppsftexportformat'] = 'File type';
+$string['csv'] = 'Comma delimited text';
+
+$string['includeheaders'] = 'Include column headers';
+$string['columnstoinclude'] = 'Columns to include';
+$string['columnidnumber'] = 'Emplid';
+$string['columngrade'] = 'Grade';
+$string['columnlastaccess'] = 'Last date of attendance<br />(last date student accessed course site)';
+$string['columnname'] = 'Name';
+$string['columngradingbasis'] = "Student's grading basis";
+$string['lastaccess'] = 'Last access';
+
+$string['nogradableclasses'] = 'There are no gradable PeopleSoft enrollments associated with this course.';
+
+$string['firstpageexportinstructions'] = 'This version of the gradebook export is intended to facilitate loading grades into PeopleSoft. This export will  provide you with the data needed to create a file according to the requirements here:
+<br /><br />
+<a href="http://onestop.umn.edu/Help/howdoiupload.htm">http://onestop.umn.edu/Help/howdoiupload.htm</a>
+<br /><br />
+For every PeopleSoft class that is associated to this Moodle course site you will get a separate file in the download.  Each file needs to be uploaded separately into PeopleSoft.';
+

grade/export/ppsft/ppsft_grade_export_form.php
@@ -1,0 +1,66 @@
+<?php
+
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
+}
+
+require_once $CFG->libdir.'/formslib.php';
+
+class ppsft_grade_export_form extends moodleform {
+    function definition() {
+        global $CFG, $COURSE, $USER, $DB;
+
+        $mform =& $this->_form;
+        $classes = $this->_customdata['classes'];
+
+        $mform->addElement('header', 'options', get_string('options', 'grades'));
+
+        // Must use only letters for this report.  The value from this element is not actually used anywhere.
+        $options = array(GRADE_DISPLAY_TYPE_LETTER     => get_string('letter', 'grades'));
+        $mform->addElement('select', 'display[letter]', get_string('gradeexportdisplaytype', 'grades'), $options);
+        $mform->setDefault('display[letter]', $CFG->grade_export_displaytype == GRADE_DISPLAY_TYPE_LETTER);
+
+        $mform->addElement('static',
+                           'ppsftexportformat',
+                           get_string('ppsftexportformat', 'gradeexport_ppsft'),
+                           get_string('csv', 'gradeexport_ppsft'));
+
+        $mform->addElement('advcheckbox', 'includeheaders', get_string('includeheaders', 'gradeexport_ppsft'));
+
+        $radio = array();
+        $courseIdentifier = null;
+        if(!empty($classes)){
+            foreach ($classes as $ppsftclass) {
+                $courseIdentifier = $ppsftclass->subject . ' ' . $ppsftclass->catalog_nbr . ' ' . $ppsftclass->section;
+                $radio[] =& $mform->createElement('radio', 'ppsftclassid', '', $courseIdentifier, $ppsftclass->id);
+            }
+            //set the first class as default
+            $mform->setDefault('ppsftclassid', array_values($classes)[0]->id);
+        }
+        $mform->addGroup($radio, 'ppsftclassid','Class', ' ', false);
+
+        $mform->addElement('header', 'columnstoinclude', get_string('columnstoinclude', 'gradeexport_ppsft'));
+
+        $mform->addElement('advcheckbox', 'includeidnumber', get_string('columnidnumber', 'gradeexport_ppsft'));
+        $mform->addElement('advcheckbox', 'includegrade', get_string('columngrade', 'gradeexport_ppsft'));
+        $mform->addElement('advcheckbox', 'includelastaccess', get_string('columnlastaccess', 'gradeexport_ppsft'));
+        $mform->addElement('advcheckbox', 'includename', get_string('columnname', 'gradeexport_ppsft'));
+        $mform->addElement('advcheckbox', 'includegradingbasis', get_string('columngradingbasis', 'gradeexport_ppsft'));
+
+        // We do not actually use the value from the idnumber, grade, and lastaccess
+        // checkboxes.  The PeopleSoft should always include those columns.  The
+        // purpose of the checkboxes is to make that clear to the user.
+        $mform->setDefault('includeidnumber', 1);
+        $mform->setDefault('includegrade', 1);
+        $mform->setDefault('includelastaccess', 1);
+        $mform->freeze('includeidnumber');
+        $mform->freeze('includegrade');
+        $mform->freeze('includelastaccess');
+
+        $mform->addElement('hidden', 'id', $COURSE->id);
+        $mform->setType('id', PARAM_INT);
+        $this->add_action_buttons(false, get_string('download'));
+
+    }
+}
+

grade/export/ppsft/ppsft_grade_export_iterator.php
@@ -1,0 +1,344 @@
+<?php
+
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
+}
+
+// Started from graded_users_iterator in grade/lib.php.
+
+/**
+ * This class iterates over all users that are graded in a course.
+ * Returns detailed info about users and their grades.
+ * Started with grade_users_iterator.  Supports filtering by PeopleSoft
+ * class.
+ */
+class ppsft_grade_export_iterator {
+
+    /**
+     * The course whose users we are interested in
+     */
+    protected $course;
+
+    /**
+     * An array of grade items or null if only user data was requested
+     */
+    protected $grade_items;
+
+    /**
+     * The ppsft_classes ID we are interested in. 0 means all.
+     */
+    protected $ppsftclassid;
+
+    /**
+     * A recordset of graded users
+     */
+    protected $users_rs;
+
+    /**
+     * A recordset of user grades (grade_grade instances)
+     */
+    protected $grades_rs;
+
+    /**
+     * Array used when moving to next user while iterating through the grades recordset
+     */
+    protected $gradestack;
+
+    /**
+     * The first field of the users table by which the array of users will be sorted
+     */
+    protected $sortfield1;
+
+    /**
+     * Should sortfield1 be ASC or DESC
+     */
+    protected $sortorder1;
+
+    /**
+     * The second field of the users table by which the array of users will be sorted
+     */
+    protected $sortfield2;
+
+    /**
+     * Should sortfield2 be ASC or DESC
+     */
+    protected $sortorder2;
+
+    /**
+     * Constructor
+     *
+     * @param object $course A course object
+     * @param array  $grade_items array of grade items, if not specified only user info returned
+     * @param int    $ppsftclassid iterate only ppsftclass users if present
+     * @param string $sortfield1 The first field of the users table by which the array of users will be sorted
+     * @param string $sortorder1 The order in which the first sorting field will be sorted (ASC or DESC)
+     * @param string $sortfield2 The second field of the users table by which the array of users will be sorted
+     * @param string $sortorder2 The order in which the second sorting field will be sorted (ASC or DESC)
+     */
+    public function __construct($course, $grade_items=null, $ppsftclassid=0,
+                                          $sortfield1='lastname', $sortorder1='ASC',
+                                          $sortfield2='firstname', $sortorder2='ASC') {
+        $this->course      = $course;
+        $this->grade_items = $grade_items;
+        $this->sortfield1  = $sortfield1;
+        $this->sortorder1  = $sortorder1;
+        $this->sortfield2  = $sortfield2;
+        $this->sortorder2  = $sortorder2;
+        $this->ppsftclassid = $ppsftclassid;
+
+        $this->gradestack  = array();
+    }
+
+    /**
+     * Initialise the iterator
+     *
+     * @return boolean success
+     */
+    public function init() {
+        global $CFG, $DB;
+
+        $this->close();
+
+        export_verify_grades($this->course->id);
+        $course_item = grade_item::fetch_course_item($this->course->id);
+        if ($course_item->needsupdate) {
+            // can not calculate all final grades - sorry
+            return false;
+        }
+
+        $coursecontext = context_course::instance($this->course->id);
+        $parentcontextids = $coursecontext->get_parent_context_ids(true);
+        $relatedcontexts = ' IN ('.implode(',', $parentcontextids).')';
+
+        list($gradebookroles_sql, $params) =
+            $DB->get_in_or_equal(explode(',', $CFG->gradebookroles), SQL_PARAMS_NAMED, 'grbr');
+        list($enrolledsql, $enrolledparams) = get_enrolled_sql($coursecontext);
+
+        $params = array_merge($params, $enrolledparams);
+
+        $ppsftclassfiltersql = '';
+        if ($this->ppsftclassid) {
+            $ppsftclassfiltersql = "AND pce.ppsftclassid = :ppsftclassid";
+            $params['ppsftclassid'] = $this->ppsftclassid;
+        }
+
+        if (empty($this->sortfield1)) {
+            // we must do some sorting even if not specified
+            $ofields = ", u.id AS usrt";
+            $order   = "usrt ASC";
+
+        } else {
+            $ofields = ", u.$this->sortfield1 AS usrt1";
+            $order   = "usrt1 $this->sortorder1";
+            if (!empty($this->sortfield2)) {
+                $ofields .= ", u.$this->sortfield2 AS usrt2";
+                $order   .= ", usrt2 $this->sortorder2";
+            }
+            if ($this->sortfield1 != 'id' and $this->sortfield2 != 'id') {
+                // user order MUST be the same in both queries,
+                // must include the only unique user->id if not already present
+                $ofields .= ", u.id AS usrt";
+                $order   .= ", usrt ASC";
+            }
+        }
+
+        $userfields = 'u.*';
+
+        // $params contents: gradebookroles and ppsftclassid (for $ppsftclassfiltersql)
+        $users_sql = "SELECT $userfields $ofields,
+                             ppsft_enrol.section AS ppsft_section,
+                             ppsft_enrol.grading_basis AS ppsft_grading_basis,
+                             la.timeaccess as last_access
+                        FROM {user} u
+                        JOIN ($enrolledsql) je ON je.id = u.id
+                        JOIN (
+                                  SELECT DISTINCT ra.userid
+                                    FROM {role_assignments} ra
+                                   WHERE ra.roleid $gradebookroles_sql
+                                     AND ra.contextid $relatedcontexts
+                             ) rainner ON rainner.userid = u.id " .
+
+                       // The following is a left join in graded_user_iterator.  In this case,
+                       // we want only ppsft enrollees.
+                       "JOIN (
+                           SELECT pce.userid AS userid,
+                                CONCAT(ppsft_classes.subject,
+                                       ppsft_classes.catalog_nbr, '_',
+                                       ppsft_classes.section) AS section,
+                                pce.grading_basis
+                           FROM {ppsft_class_enrol} pce
+                                JOIN {ppsft_classes} ppsft_classes
+                                    ON ppsft_classes.id = pce.ppsftclassid
+                                JOIN {enrol_umnauto_classes} enrol_umnauto_classes
+                                    ON ppsft_classes.id = enrol_umnauto_classes.ppsftclassid
+                                JOIN {enrol} enrol
+                                    ON enrol_umnauto_classes.enrolid = enrol.id
+                           WHERE  enrol.courseid = :courseid AND
+                                  pce.status = 'E' AND
+                                  pce.grading_basis not in ('NON', 'NGA')
+                                  $ppsftclassfiltersql
+                           GROUP BY userid
+                       ) ppsft_enrol ON u.id = ppsft_enrol.userid
+                       LEFT JOIN {user_lastaccess} la on la.userid = u.id and
+                                                         la.courseid = :courseid2
+                       WHERE u.deleted = 0
+                       ORDER BY {$order}";
+
+        $params['courseid'] = $this->course->id;
+        $params['courseid2'] = $this->course->id;
+
+        $this->users_rs = $DB->get_recordset_sql($users_sql, $params);
+
+        if (!empty($this->grade_items)) {
+            $itemids = array_keys($this->grade_items);
+            list($itemidsql, $grades_params) = $DB->get_in_or_equal($itemids, SQL_PARAMS_NAMED, 'items');
+            $params = array_merge($params, $grades_params);
+            // $params contents: gradebookroles, enrolledparams, ppsftclassid (for $ppsftclassfiltersql) and itemids
+
+            $grades_sql = "SELECT g.* $ofields
+                             FROM {grade_grades} g
+                             JOIN {user} u ON g.userid = u.id
+                             JOIN ($enrolledsql) je ON je.id = u.id
+                             JOIN (
+                                      SELECT DISTINCT ra.userid
+                                        FROM {role_assignments} ra
+                                       WHERE ra.roleid $gradebookroles_sql
+                                         AND ra.contextid $relatedcontexts
+                                  ) rainner ON rainner.userid = u.id
+                             JOIN (
+                                 SELECT pce.userid AS userid
+                                 FROM {ppsft_class_enrol} pce
+                                      JOIN {enrol_umnauto_classes} euc
+                                          ON euc.ppsftclassid = pce.ppsftclassid
+                                      JOIN {enrol} e
+                                          ON e.id = euc.enrolid
+                                 WHERE  e.courseid = :courseid AND
+                                        pce.status = 'E'
+                                        $ppsftclassfiltersql
+                                 GROUP BY userid
+                             ) ppsft_enrol ON u.id = ppsft_enrol.userid
+                             WHERE u.deleted = 0
+                               AND g.itemid $itemidsql
+                         ORDER BY $order, g.itemid ASC";
+            $this->grades_rs = $DB->get_recordset_sql($grades_sql, $params);
+        } else {
+            $this->grades_rs = false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns information about the next user
+     * @return mixed array of user info, all grades or null when no more users found
+     */
+    public function next_user() {
+        if (!$this->users_rs) {
+            return false; // no users present
+        }
+
+        if (!$this->users_rs->valid()) {
+            if ($current = $this->_pop()) {
+                // this is not good - user or grades updated between the two reads above :-(
+            }
+
+            return false; // no more users
+        } else {
+            $user = $this->users_rs->current();
+            $this->users_rs->next();
+        }
+
+        // find grades of this user
+        $grade_records = array();
+        while (true) {
+            if (!$current = $this->_pop()) {
+                break; // no more grades
+            }
+
+            if (empty($current->userid)) {
+                break;
+            }
+
+            if ($current->userid != $user->id) {
+                // grade of the next user, we have all for this user
+                $this->_push($current);
+                break;
+            }
+
+            $grade_records[$current->itemid] = $current;
+        }
+
+        $grades = array();
+
+        if (!empty($this->grade_items)) {
+            foreach ($this->grade_items as $grade_item) {
+                if (array_key_exists($grade_item->id, $grade_records)) {
+                    $grades[$grade_item->id] = new grade_grade($grade_records[$grade_item->id], false);
+                } else {
+                    $grades[$grade_item->id] =
+                        new grade_grade(array('userid'=>$user->id, 'itemid'=>$grade_item->id), false);
+                }
+            }
+        }
+
+        $result = new stdClass();
+        $result->user      = $user;
+        $result->grades    = $grades;
+        return $result;
+    }
+
+    /**
+     * Close the iterator, do not forget to call this function
+     */
+    public function close() {
+        if ($this->users_rs) {
+            $this->users_rs->close();
+            $this->users_rs = null;
+        }
+        if ($this->grades_rs) {
+            $this->grades_rs->close();
+            $this->grades_rs = null;
+        }
+        $this->gradestack = array();
+    }
+
+    /**
+     * Add a grade_grade instance to the grade stack
+     *
+     * @param grade_grade $grade Grade object
+     *
+     * @return void
+     */
+    private function _push($grade) {
+        array_push($this->gradestack, $grade);
+    }
+
+
+    /**
+     * Remove a grade_grade instance from the grade stack
+     *
+     * @return grade_grade current grade object
+     */
+    private function _pop() {
+        global $DB;
+        if (empty($this->gradestack)) {
+            if (empty($this->grades_rs) || !$this->grades_rs->valid()) {
+                return null; // no grades present
+            }
+
+            $current = $this->grades_rs->current();
+
+            $this->grades_rs->next();
+
+            return $current;
+        } else {
+            return array_pop($this->gradestack);
+        }
+    }
+}
+
+
+
+
+
+

commit 6cad3f2969b9a288152874e94fdfb537aff548f8

grade/export/ppsft. Fixed to export only the overall course grade.
This fixes a defect introduced by c0bd9b29ef44151d72 that resulted
in the inclusion of other grades in the course in addition to
the course-level grade.

grade/export/ppsft/export.php
@@ -21,6 +21,15 @@ $classes = grade_export_ppsft::get_graded_ppsft_classes_for_course($COURSE->id);
 
 //pass list of peoplesoft classes to be selected  by user
 $mform = new ppsft_grade_export_form(null, array( 'classes'=>$classes ));
-$export = new grade_export_ppsft($course, $mform->get_data());
+
+// The form does not include grade item selection because we always want just the overall
+// course grade for this export. Artificially set that in the form data here.
+$courseitem = grade_item::fetch_course_item($COURSE->id);
+
+$data = $mform->get_data();
+$data->itemids = array($courseitem->id => '1');
+
+//pass list of peoplesoft classes to be selected  by user
+$export = new grade_export_ppsft($course, $data);
 $export->print_grades();
 